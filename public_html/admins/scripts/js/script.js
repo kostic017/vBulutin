07ea452c7d2cd83a96612942db188ac0ae870f49
nkostic97vr <nkostic97vr@gmail.com>
2018-01-13 21:16:19 +0100

    Onemoguci u admins da se forma submituje kada se lupi Enter

--- public_html/admins/scripts/js/script.js
+++ public_html/admins/scripts/js/script.js
@@ -1,12 +1,22 @@
 $(function () {
-    $(`a[href="${getFileName()}"]`).addClass("active");
-
     setupWidgets();
     setupPositionsSaving();
     setupSortingAction();
     setupFormValidation();
     forcingParentsSectionToChildren();
+    disableFormSumbitOnEnterKeyPress();
+
+    $(`a[href="${getFileName()}"]`).addClass("active");
 });
+
+function disableFormSumbitOnEnterKeyPress() {
+	$("form input").keydown(function (e) {
+	    if (e.keyCode == 13) {
+	        e.preventDefault();
+	        return false;
+	    }
+	});
+}
 
 // ==== TABLES ====
 
