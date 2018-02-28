706da4af173f8c74a4e2ec8e4cf9bd600ff28bba
nkostic97vr <nkostic97vr@gmail.com>
2018-02-28 08:39:48 +0100

    Prelazimo na PHPStorm (dodao u gitignore)

--- public_html/playground.php
+++ public_html/playground.php
@@ -1,14 +1,2 @@
 <?php
-    require_once "shared/scripts/php/main.php";
-    
-    if (isPostRequest()) {
-        $posts = execAndFetchAssoc("SELECT * FROM posts", FETCH::ALL);
-        foreach ($posts as $post) {
-            executeQuery("UPDATE topics SET started='{$post["posted"]}', updated='{$post["posted"]}' WHERE id='{$post["topics_id"]}'");
-        }
-    }
-?>
-
-<form method="post">
-    <input type="submit" value="Go">
-</form>
+phpinfo();
