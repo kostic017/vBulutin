22d499186a6a92211d06c7e2f30c27f2bde437c8
nkostic97vr <nkostic97vr@gmail.com>
2018-04-17 02:21:48 +0200

    Povecaj responzivnost

--- resources/views/admin/sections/table.blade.php
+++ resources/views/admin/sections/table.blade.php
@@ -12,7 +12,7 @@
 
     <form action="{{ route("{$table}.index") }}" method="get">
         <div class="actions row">
-            <div class="buttons col text-nowrap">
+            <div class="buttons col">
                 <a href="{{ route("{$table}.create") }}" class="btn btn-primary">
                     {{ $table === 'categories' ? __('admin.create-category') : __('admin.create-forum') }}
                 </a>
@@ -31,7 +31,7 @@
                     </label>
                 </div>
             </div>
-            <div class="pagination justify-content-right">
+            <div class="menu col">
                 <select name="perPage" class="form-control">
                     <option value="0" {{ $perPage === 0 ? 'selected' : '' }}>&infin;</option>
                     @for ($i = $step; $i < $max; $i += $step)
@@ -42,69 +42,71 @@
         </div>
     </form>
 
-    <table class="table table-striped sections" data-name="{{ $table }}">
-        <thead class="text-nowrap">
-            <tr>
-                <th>
-                    <a href="javascript:void(0)" class="sort-link" data-column="id">
-                        ID @sort_icon('id')
-                    </a>
-                </th>
-                <th>
-                    <a href="javascript:void(0)" class="sort-link" data-column="title">
-                        {{ __('db.title') }} @sort_icon('title')
-                    </a>
-                </th>
-
-                @if ($table === 'forums')
+    <div class="table-responsive">
+        <table class="table table-striped sections" data-name="{{ $table }}">
+            <thead class="text-nowrap">
+                <tr>
                     <th>
-                        <a href="javascript:void(0)" class="sort-link" data-column="category">
-                            {{ __('db.category') }} @sort_icon('category_id')
+                        <a href="javascript:void(0)" class="sort-link" data-column="id">
+                            ID @sort_icon('id')
                         </a>
                     </th>
-                @endif
-
-                <th colspan="3">&nbsp;</th>
-            </tr>
-        </thead>
-        <tbody class="table-hover">
-            @foreach ($rows as $row)
-                <tr id="row-{{ $row->id }}">
-                    <td>{{ $row->id }}</td>
-                    <td>{{ $row->title }}</td>
+                    <th>
+                        <a href="javascript:void(0)" class="sort-link" data-column="title">
+                            {{ __('db.title') }} @sort_icon('title')
+                        </a>
+                    </th>
 
                     @if ($table === 'forums')
-                        <td>{{ $row->category_name }}</td>
+                        <th>
+                            <a href="javascript:void(0)" class="sort-link" data-column="category">
+                                {{ __('db.category') }} @sort_icon('category_id')
+                            </a>
+                        </th>
                     @endif
 
-                    <td>
-                        <a href="{{ route("{$table}.show", ["{$table}" => $row->id]) }}" class="btn btn-xs btn-success">
-                            {{ __('admin.view') }}
-                        </a>
-                    </td>
-                    <td>
-                        <a href="{{ route("{$table}.edit", ["{$table}" => $row->id]) }}" class="btn btn-xs btn-info">
-                            {{ __('admin.edit') }}
-                        </a>
-                    </td>
-                    <td>
-                        @if ($row->trashed())
-                            <form action="{{ route("{$table}.restore", ["{$table}" => $row->id]) }}" method="post">
-                                @csrf
-                                <button class="btn btn-xs btn-danger" type="submit">{{ __('admin.restore') }}</button>
-                            </form>
-                        @else
-                            <form action="{{ route("{$table}.destroy", ["{$table}" => $row->id]) }}" method="post">
-                                @csrf
-                                {{ method_field('DELETE') }}
-                                <button class="btn btn-xs btn-danger" type="submit">{{ __('admin.delete') }}</button>
-                            </form>
-                        @endif
-                    </td>
+                    <th colspan="3">&nbsp;</th>
                 </tr>
-            @endforeach
-        </tbody>
-    </table>
+            </thead>
+            <tbody class="table-hover">
+                @foreach ($rows as $row)
+                    <tr id="row-{{ $row->id }}">
+                        <td>{{ $row->id }}</td>
+                        <td>{{ $row->title }}</td>
+
+                        @if ($table === 'forums')
+                            <td>{{ $row->category_name }}</td>
+                        @endif
+
+                        <td>
+                            <a href="{{ route("{$table}.show", ["{$table}" => $row->id]) }}" class="btn btn-xs btn-success">
+                                {{ __('admin.view') }}
+                            </a>
+                        </td>
+                        <td>
+                            <a href="{{ route("{$table}.edit", ["{$table}" => $row->id]) }}" class="btn btn-xs btn-info">
+                                {{ __('admin.edit') }}
+                            </a>
+                        </td>
+                        <td>
+                            @if ($row->trashed())
+                                <form action="{{ route("{$table}.restore", ["{$table}" => $row->id]) }}" method="post">
+                                    @csrf
+                                    <button class="btn btn-xs btn-danger" type="submit">{{ __('admin.restore') }}</button>
+                                </form>
+                            @else
+                                <form action="{{ route("{$table}.destroy", ["{$table}" => $row->id]) }}" method="post">
+                                    @csrf
+                                    {{ method_field('DELETE') }}
+                                    <button class="btn btn-xs btn-danger" type="submit">{{ __('admin.delete') }}</button>
+                                </form>
+                            @endif
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 
     @if ($perPage > 0)
         <div class="row justify-content-center">
