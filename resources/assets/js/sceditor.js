133fee02d05a10a951e4b4ac4cb57ee0f4882ce4
nkostic97vr <nkostic97vr@gmail.com>
2018-04-30 07:08:03 +0200

    Eksplicitno toolbar podesavanje za SCEditor
    Kako bi kasnije mogao lako da dodam svoje komande u toolbar.

--- resources/assets/js/sceditor.js
+++ resources/assets/js/sceditor.js
@@ -1,17 +1,22 @@
 function initSceditor() {
-    sceditor.create(document.querySelector('.sceditor'), {
+    sceditor.create(document.querySelector(".sceditor"), {
         width: "100%",
         height: "300px",
         format: "bbcode",
         bbcodeTrim: true,
         spellcheck: false,
         resizeWidth: false,
-        toolbarExclude: 'email',
+        toolbarExclude: "email",
         resizeMinHeight: "120px",
         resizeMaxHeight: "1000px",
         locale: $("html").attr("lang"),
         emoticonsRoot: "/lib/sceditor/",
         plugins: "undo",
+        toolbar: "bold,italic,underline,strike,subscript,superscript|" +
+            "left,center,right,justify|font,size,color,removeformat|" +
+            "cut,copy,pastetext|bulletlist,orderedlist,indent,outdent|" +
+            "table|bbcodetag,code,quote|horizontalrule,image,email,link,unlink|" +
+            "emoticon,youtube,date,time|ltr,rtl|print,maximize,source",
         pastetext: {
             enabled: true,
             addButton: true
