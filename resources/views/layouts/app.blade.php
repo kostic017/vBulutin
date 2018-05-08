d2f7066d1c444cbe254a1b3eabe246bcab0f6b36
nkostic97vr <nkostic97vr@gmail.com>
2018-05-08 08:08:25 +0200

    Dropdown meni vise ne izlazi van stranice

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -75,7 +75,7 @@
                                     <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                         {{ Auth::user()->name ?? Auth::user()->username }} <span class="caret"></span>
                                     </a>
-                                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                         <a class="dropdown-item" href="profile.php?id={{ Auth::id() }}">
                                             <i class="fas fa-user"></i> Profil
                                         </a>
