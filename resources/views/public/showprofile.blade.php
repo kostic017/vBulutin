023bef82d96542d2fa7ae95473c0408285c97a7d
nkostic97vr <nkostic97vr@gmail.com>
2018-06-02 14:39:05 +0200

    Prikazi sve informacije o korisniku

--- resources/views/public/showprofile.blade.php
+++ resources/views/public/showprofile.blade.php
@@ -1,32 +1,44 @@
 @extends('layouts.public')
 
 @section('content')
-    <div class="edit-profile bgc-main p-main">
+    <div class="show-profile bgc-main p-main">
         <h2 class="username">{{ $user->username }}</h2>
-
-        <div class="avatar">
-            @avatar(big)
-        </div>
-
-        <div class="info">
-            <p>
-                <b>Pridružio</b><br>
-                {{ extractDate($user->registered_at) }}
-            </p>
-            <p>
-                <b>Ukupno poruka</b><br>
-                {{ $user->posts()->get()->count() }}
-            </p>
-            <p>
+        <section>
+            <div class="avatar">
+                @avatar(big)
+            </div>
+            {{-- <p>
                 <a href="">Sve poruke korisnika {{ $user->username }}</a><br>
                 <a href="">Sve teme koje je započeo korisnik {{ $user->username }}</a><br>
                 <a href="">Sve teme u kojima je učestvovao korisnik {{ $user->username }}</a>
-            </p>
-        </div>
-
+            </p> --}}
+            <dl>
+                <dt>Pridružio</dt>
+                <dd>{{ extractDate($user->registered_at) }}</dd>
+                <dt>Ukupno poruka</b><dt>
+                <dd>{{ $user->posts()->get()->count() }}</dd>
+                <dt>Pol</dt>
+                <dd>{{ $profile->sex ?? '-' }}</dd>
+            </dl>
+            <dl>
+                <dt>Datum rođenja</dt>
+                <dd>{{ $profile->birthday_on ?? '-' }}</dd>
+                <dt>Mesto rođenja</dt>
+                <dd>{{ $profile->birthplace ?? '-' }}</dd>
+                <dt>Prebivalište</dt>
+                <dd>{{ $profile->residence ?? '-' }}</dd>
+                <dt>Zanimanje</dt>
+                <dd>{{ $profile->job ?? '-' }}</dd>
+            </dl>
+            <dl>
+                <dt>O meni</dt>
+                <dd>{{ $profile->about ?? '-' }}</dd>
+                <dt>Potpis</dt>
+                <dd>{{ $profile->signature ?? '-' }}</dd>
+            </dl>
+        </section>
         @if ($user->id == Auth::id() || Auth::user()->is_admin)
             <a class="btn btn-success" href="{{ route('public.profile.edit', ['profile' => $user->username]) }}">Izmeni profil</a>
         @endif
-
     </div>
 @stop
