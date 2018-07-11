cbc8d58097c623c10e9f4c43718c004bcc2f6f2e
nkostic97vr <nkostic97vr@gmail.com>
2018-07-11 11:29:18 +0200

    Dodaj 'use' gde fali

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers\Auth;
 
 use App\Notifications\ResetPassword;
 use App\Http\Controllers\Controller;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Password;
 use Illuminate\Foundation\Auth\ResetsPasswords;
 
 class ResetPasswordController extends Controller {
